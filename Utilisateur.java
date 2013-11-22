8890081a484aabbfbe6ee2a7079b523ebe2b8a29
Juliana Alvarado <julianamac16@hotmail.com>
2013-11-21 20:55:46 -0500

    class utilisateur
    Class utilisateur

---
+++ Utilisateur.java
@@ -0,0 +1,19 @@
+
+
+package com.eecs.seg2505_2013;
+
+import java.util.*;
+import java.io.*;
+
+public class Utilisateur  {
+
+	
+	Private Object User
+	
+	public Utilisateur ()
+	
+	{
+		This=null;
+		
+	}
+}
